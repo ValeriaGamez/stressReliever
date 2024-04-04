dca743c13143c1f0be6a22e0d90285dc90e3d4b7
Malachi Dewitt <dewitt851@gmail.com>
2024-04-03 20:42:00 -0700

    Added body backend

---
+++ popup/body.js
@@ -0,0 +1,41 @@
+// Body image (the potato)
+var mpmBody = document.createElement("img");
+mpmBody.src = "../images/Temp Art/bodyTEMP.png";
+document.getElementById("mpm-body").appendChild(mpmBody);
+mpmBody.style.height = "100%";
+mpmBody.style.width = "100%";
+
+// Hat image
+var mpmHat = document.createElement("img");
+mpmHat.src = "../images/Temp Art/legTEMP.png";
+document.getElementById("mpm-hat").appendChild(mpmHat);
+mpmHat.style.height = "100%";
+mpmHat.style.width = "100%";
+
+// Left Arm image
+var mpmLeftArm = document.createElement("img");
+mpmLeftArm.src = "../images/Temp Art/rightArmTEMP.png";
+document.getElementById("mpm-left-arm").appendChild(mpmLeftArm);
+mpmLeftArm.style.height = "100%";
+mpmLeftArm.style.width = "100%";
+
+// Right Arm image
+var mpmRightArm = document.createElement("img");
+mpmRightArm.src = "../images/Temp Art/rightArmTEMP.png";
+document.getElementById("mpm-right-arm").appendChild(mpmRightArm);
+mpmRightArm.style.height = "100%";
+mpmRightArm.style.width = "100%";
+
+// Left Foot image
+var mpmLeftFoot = document.createElement("img");
+mpmLeftFoot.src = "../images/Temp Art/legTEMP.png";
+document.getElementById("mpm-left-foot").appendChild(mpmLeftFoot);
+mpmLeftFoot.style.height = "100%";
+mpmLeftFoot.style.width = "100%";
+
+// Right Foot image
+var mpmRightFoot = document.createElement("img");
+mpmRightFoot.src = "../images/Temp Art/legTEMP.png";
+document.getElementById("mpm-right-foot").appendChild(mpmRightFoot);
+mpmRightFoot.style.height = "100%";
+mpmRightFoot.style.width = "100%";
